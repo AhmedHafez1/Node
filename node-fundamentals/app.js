598be708fb534f7299240e60608cf61ad3ce878f
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-01 00:27:04 +0300

    req url

--- node-fundamentals/app.js
+++ node-fundamentals/app.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write('Welcome to node');
-  res.end();
+  if (req.url === '/') {
+    res.end('Home Page');
+    return;
+  }
+  if (req.url === '/about') {
+    res.end('About Page');
+    return;
+  }
+  res.end(`<h1>Page not found</h1>`);
 });
 
 server.listen(5000);
