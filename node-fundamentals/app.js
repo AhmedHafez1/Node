c11b802ebafe9c822331d4233d0cb2d3198bb300
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-05 06:39:22 +0300

    Event emitters

--- node-fundamentals/app.js
+++ node-fundamentals/app.js
@@ -1,37 +1,6 @@
-const { readFile, writeFile } = require('fs').promises;
-const util = require('util');
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+emitter.on('fired', () => console.log('Event fired successfully'));
 
-// With async/await
-async function printText() {
-  try {
-    const firstText = await readFile('./content/file1.txt', 'utf8');
-    const secondText = await readFile('./content/file2.txt', 'utf8');
-
-    await writeFile(
-      './content/result-async-patterns.txt',
-      `Hello here is the data: ${firstText} ${secondText}\n`,
-      { flag: 'a' }
-    );
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-printText();
-
-// const getText = (path) => {
-//   return new Promise((res, rej) => {
-//     readFile(path, 'utf8', (data, err) => {
-//       if (err) rej(err);
-//       else res(data);
-//     });
-//   });
-// };
-
-// With then/catch chaining
-// getText('./content/file1.txt')
-//   .then((text) => console.log(text))
-//   .catch((err) => console.error(err));
+emitter.emit('fired');
