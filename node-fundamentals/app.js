2dc1d5544a954f71967e0c017843953b13748ede
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-05 06:45:27 +0300

    events with data

--- node-fundamentals/app.js
+++ node-fundamentals/app.js
@@ -1,6 +1,9 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
+emitter.on('fired', (children) =>
+  console.log(`Event fired successfully. Thanks ${children}`)
+);
 emitter.on('fired', () => console.log('Event fired successfully'));
 
-emitter.emit('fired');
+emitter.emit('fired', 'Omar Mariam Yosuf');
