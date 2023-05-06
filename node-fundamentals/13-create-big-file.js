6330c3c26d32daa30fcfce452dd393eede3c5045
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-06 18:18:28 +0300

    stream encoding and chunk size

--- node-fundamentals/13-create-big-file.js
+++ node-fundamentals/13-create-big-file.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 
 for (let index = 0; index < 100000; index++) {
-  fs.writeFileSync('./content/big-file.txt', `Heloooooooooooooooooo ${index}`, {
-    flag: 'a',
-  });
+  fs.writeFileSync(
+    './content/big-file.txt',
+    `Heloooooooooooooooooo ${index}\n`,
+    {
+      flag: 'a',
+    }
+  );
 }
--- node-fundamentals/app.js
+++ node-fundamentals/app.js
@@ -1,5 +1,8 @@
 const { createReadStream } = require('fs');
 
-const stream = createReadStream('./content/big-file.txt');
+const stream = createReadStream('./content/big-file.txt', {
+  highWaterMark: 80,
+  encoding: 'utf8',
+});
 
-stream.on('data', (chunk) => console.log(chunk.byteLength));
+stream.on('data', (chunk) => console.log(chunk));
