2553c2cefa4a60c98bcbfd2a7c04480f8153e440
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-06 17:57:56 +0300

    create big file with file system

---
+++ node-fundamentals/13-create-big-file.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+for (let index = 0; index < 100000; index++) {
+  fs.writeFileSync('./content/big-file.txt', `Heloooooooooooooooooo ${index}`, {
+    flag: 'a',
+  });
+}
